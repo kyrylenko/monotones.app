8711a07195e941f3da334b4c99ec8954fb3b0807
Pavlo Kyrylenko <Pavlo_Kyrylenko@epam.com>
2018-09-26 18:18:33 +0300

    MVP works

--- src/App.js
+++ src/App.js
@@ -7,41 +7,6 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    //this.url = 'http://streaming.tdiradio.com:8000/house.mp3'
-    //this.url = require('./assets/sounds/bleu_whales.mp3')
-    this.url = require('./assets/sounds/deep_space.mp3')
-    this.stream = new Audio(this.url);
-    //this.stream.loop = true;
-    this.stream.preload = 'none';
-
-    this.stream.addEventListener('timeupdate', function () {
-      var buffer = .44
-      if (this.currentTime > this.duration - buffer) {
-        this.currentTime = 0
-        this.play()
-      }
-    }, false);
-  }
-
-  play = () => {
-    console.log("plat click");
-    this.stream.play();
-  }
-
-  pause = () => {
-    this.stream.pause();
-    this.stream.src = ''
-    this.stream.load();
-
-    this.stream = null;
-
-    this.stream = new Audio();
-    this.stream.src = this.url;
-    this.stream.preload = 'none';
-    this.stream.pause();
-  }
 
   render() {
     return (
@@ -49,44 +14,36 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Monotones</h1>
         </header>
         <Container>
           <Row>
             <Col lg={2} md={2} sm={2} className="hidden-xs"></Col>
             <Col lg={3} md={3} sm={4} xs={6} className="text-center">
-              <img alt="Rain" className="sound sound-left" data-element="sound" data-name="rain" src={require('./assets/icons/bleu-whales.png')} title="Rain" style={{ opacity: 1 }}></img>
-              <div className="to-left ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" data-element="volume_slider" data-element-name="rain_slider" style={{ display: 'block' }}>
-                <span className="ui-slider-handle ui-state-default ui-corner-all" style={{ flex: 0.6 }}></span>
-              </div>
+              <SoundSlider src={require('./assets/icons/deep-space.png')} sound={require('./assets/sounds/deep_space.ogg')} enabled={false} />
             </Col>
             <Col lg={2} md={2} sm={2} className="hidden-xs hidden-sm"></Col>
             <Col lg={3} md={3} sm={4} xs={6} className="text-center">
-              <audio controls loop>
-                <source src={require('./assets/sounds/deep_space.ogg')} type="audio/ogg"></source>
-              </audio>
+              <SoundSlider src={require('./assets/icons/bleu-whales.png')} sound={require('./assets/sounds/bleu_whales.mp3')} enabled={false} />
             </Col>
             <Col lg={2} md={2} sm={2} className="hidden-xs"></Col>
           </Row>
           <Row>
             <Col lg={2} md={2} sm={2} className="hidden-xs"></Col>
             <Col lg={3} md={3} sm={4} xs={6} className="text-center">
-              <audio controls loop>
-                <source src={require('./assets/sounds/bleu_whales.mp3')} type="audio/ogg"></source>
-              </audio>
+              <SoundSlider src={require('./assets/icons/campfire.png')} sound={require('./assets/sounds/campfire.mp3')} enabled={true} />
             </Col>
             <Col lg={2} md={2} sm={2} className="hidden-xs hidden-sm"></Col>
             <Col lg={3} md={3} sm={4} xs={6} className="text-center">
-              <audio controls loop>
-                <source src={require('./assets/sounds/deep_space.ogg')} type="audio/ogg"></source>
-              </audio>
+              <SoundSlider src={require('./assets/icons/farm.png')} sound={require('./assets/sounds/farm.ogg')} enabled={false} />
             </Col>
             <Col lg={2} md={2} sm={2} className="hidden-xs"></Col>
           </Row>
         </Container>
 
-        <button onClick={() => this.play()}>Play</button>
-        <button onClick={() => this.pause()}>Pause</button>
+{/*         <audio controls loop>
+          <source src={require('./assets/sounds/bleu_whales.mp3')} type="audio/ogg"></source>
+        </audio> */}
       </div>
     );
   }
--- src/components/SoundSlider.js
+++ src/components/SoundSlider.js
@@ -1,5 +1,114 @@
 import React, { Component } from 'react';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import '../App.css';
 
 export class SoundSlider extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPlay: this.props.enabled,
+            volume: 0.7
+        };
+        //this.url = 'http://streaming.tdiradio.com:8000/house.mp3'
+        //this.url = require('./assets/sounds/bleu_whales.mp3')
+
+        this.stream = new Audio(this.props.sound);
+        //this.stream.loop = true;
+        this.stream.preload = 'none';
+        this.stream.volume = this.state.volume;
+
+        this.stream.addEventListener('timeupdate', function () {
+            var buffer = .44
+            if (this.currentTime > this.duration - buffer) {
+                this.currentTime = 0
+                this.play()
+            }
+        }, false);
+    }
+
+    onSliderChange = (volume) => {
+        console.log("onSliderChange: ", volume);
+        this.setState({ volume });
+    }
+
+    clickHandler = () => {
+        console.log("clickHandler: ");
+        this.setState({ isPlay: !this.state.isPlay })
+    }
+
+    play = () => {
+        console.log("play");
+        this.stream.play();
+    }
+
+    pause = () => {
+        console.log("pause");
+        this.stream.pause();
+        this.stream.src = ''
+        this.stream.load();
+
+        this.stream = null;
+
+        this.stream = new Audio();
+        this.stream.src = this.props.sound;
+        this.stream.preload = 'none';
+        this.stream.pause();
+    }
+
+    playPause() {
+        if (this.state.isPlay) {// && this.stream.paused
+            this.play();
+        } else {
+            this.pause();
+        }
+    }
+
+    componentDidMount() {        
+        this.playPause();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("playPause, isPlay, this.stream.paused, volume ", this.state.isPlay, this.stream.paused, this.state.volume);
+        this.stream.volume = this.state.volume;
+
+        if (prevState.isPlay !== this.state.isPlay) {
+            this.playPause();
+        }
+    }
+
+    render() {
+        return (
+            <div className="slider">
+                <img alt="Rain" className="sound-icon" data-element="sound" data-name="rain" src={this.props.src} title="Rain"
+                    style={{ opacity: this.state.isPlay ? 1 : 0.5 }}
+                    onClick={() => this.clickHandler()}>
+                </img>
+                <Slider
+                    max={1}
+                    step={0.01}
+                    onChange={this.onSliderChange}
+                    //defaultValue={0.7}
+                    value={this.state.volume}
+                    trackStyle={{ height: 10 }}
+                    railStyle={{ height: 10 }}
+                    handleStyle={{
+                        //borderColor: 'blue',
+                        height: 28,
+                        width: 28,
+                        marginLeft: -14,
+                        marginTop: -9,
+                        //backgroundColor: 'black',
+                    }}
+                    style={{ visibility: this.state.isPlay ? 'visible' : 'hidden' }} />
+
+
+
+                {/*<div className="to-left ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" data-element="volume_slider" data-element-name="rain_slider" style={{ display: 'block' }}>
+                    <span className="ui-slider-handle ui-state-default ui-corner-all" style={{ flex: 0.6 }}></span>                    
+                </div> */}
+            </div>
+        );
+    }
 }
