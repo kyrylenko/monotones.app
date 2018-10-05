665c45a1a4f6516b884bb63261c1771840ab7415
Pavlo Kyrylenko <Pavlo_Kyrylenko@epam.com>
2018-10-05 12:37:47 +0300

    removed Monotones from footer

--- src/Layout.js
+++ src/Layout.js
@@ -12,19 +12,19 @@ class Layout extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{constants.appName}</h1>          
+          <h1 className="App-title">{constants.appName}</h1>
         </header>
         {this.props.children}
 
         <div className="footer-logo-welcome">
           <div className="container">
             <div className="row">
-              <div className="hidden-lg hidden-md hidden-sm col-xs-1 text-center"></div>
+              {/*<div className="hidden-lg hidden-md hidden-sm col-xs-1 text-center"></div>
               <div className="col-lg-2 col-md-2 col-sm-12 col-xs-10 footer-logo">
                 <a href="https://monotones.app/">{constants.appName}</a>
               </div>
-              <div className="hidden-lg hidden-md hidden-sm col-xs-1 text-center"></div>
-              <div className="col-lg-10 col-md-10 col-sm-12 col-xs-12 footer-logo-menu">
+              <div className="hidden-lg hidden-md hidden-sm col-xs-1 text-center"></div> */}
+              <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-logo-menu">
                 <ul style={{ marginBottom: 0 }}>
                   {/* <li><a target="_blank" rel="noopener noreferrer" href="/about">About/Features</a></li> */}
                   <li><Link to='about' target="_blank">About</Link> </li>
