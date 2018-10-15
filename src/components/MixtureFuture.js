d0fbfd8c3ee2f633e16b1fd77eb05c40cb80349a
kashtan <kirilenko.pavlo@gmail.com>
2018-10-15 22:52:05 +0300

    mixture animation added

--- src/components/MixtureFuture.js
+++ src/components/MixtureFuture.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
+import { CSSTransitionGroup } from 'react-transition-group';
 import save from '../assets/icons/save.svg'
 import { StringUtils } from '../utils/StringUtils'
 
@@ -16,14 +17,19 @@ export default class MixtureFuture extends Component {
                     src={require(`../assets/icons/white/${x.id}.png`)}
                     id={x.id}
                     alt={x.id}
-                    title={utils.idToTitle(x.id)}
+                    title={`Pause ${utils.idToTitle(x.id)}`}
                     onClick={() => this.props.pauseSound(x.id)}></img>
             </div>);
 
         return (
             <Row style={{ marginTop: '5px' }}>
-                <Col lg={9} md={9} sm={9} xs={9} className="mixture-block flex-container" style={{ overflow: 'hidden' }}>
-                    {sounds}
+                <Col lg={9} md={9} sm={9} xs={9} className="mixture-block" style={{ overflow: 'hidden' }}>
+                    <CSSTransitionGroup className="flex-container"
+                        transitionName="mixanim"
+                        transitionEnterTimeout={400}
+                        transitionLeaveTimeout={400}>
+                        {sounds}
+                    </CSSTransitionGroup>
                 </Col>
                 <Col lg={3} md={3} sm={3} xs={3}>
                     <img className="mixture-img" src={save} title='Save mixture' alt='Save mixture' onClick={this.props.saveClick}></img>
