931dadd9e8fb5d281fe73390c802df8e52137d76
kashtan <kirilenko.pavlo@gmail.com>
2018-11-11 09:57:01 +0200

    tiny routing correction

--- src/Layout.js
+++ src/Layout.js
@@ -22,8 +22,8 @@ class Layout extends Component {
               <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-logo-menu">
                 <ul style={{ marginBottom: 0 }}>
                   {/* <li><a target="_blank" rel="noopener noreferrer" href="/about">About/Features</a></li> */}
-                  <li><Link to='about' target="_blank">About</Link> </li>
-                  <li><Link to='terms' target="_blank">Terms &amp; Privacy</Link> </li>
+                  <li><Link to='/about' target="_blank">About</Link> </li>
+                  <li><Link to='/terms' target="_blank">Terms &amp; Privacy</Link> </li>
                   <li><a target="_blank" rel="noopener noreferrer" href="mailto:contact@monotones.app">Contact</a></li>
                 </ul>
               </div>
