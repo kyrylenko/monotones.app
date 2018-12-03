15ab908b7a98df5afefef517021dff6fd69d48a0
kashtan <kirilenko.pavlo@gmail.com>
2018-12-03 21:35:48 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-//import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import { PersistGate } from 'redux-persist/es/integration/react'
 import { Provider } from 'react-redux';
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,4 @@
 import { combineReducers, createStore } from 'redux';
-//import { routerReducer } from 'react-router-redux';
-//import { persistStore, persistCombineReducers, } from 'redux-persist';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import { createBlacklistFilter } from 'redux-persist-transform-filter';
