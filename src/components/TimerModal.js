594aa84a71b7144937b97240274eda3e8763a384
kashtan <kirilenko.pavlo@gmail.com>
2019-01-20 19:32:01 +0200

    timer modal correction

--- src/components/TimerModal.js
+++ src/components/TimerModal.js
@@ -5,11 +5,11 @@ import { secToMin } from '../utils/Utils';
 export default class TimerModal extends React.Component {
 
     state = {
-        interval: '1'//min
+        interval: '60'//sec 
     };
 
     start = () => {
-        this.props.start(this.state.interval * 60);
+        this.props.start(this.state.interval);
         this.props.toggle();
     };
 
@@ -27,26 +27,32 @@ export default class TimerModal extends React.Component {
     scroll = (e) => { };
 
     render() {
-        const { minutes, seconds } = secToMin(this.props.interval);
+        console.log(this.state.interval)
+        const { minutes, seconds } = secToMin(this.props.timerRun ? this.props.interval : this.state.interval);
 
-        const comp = this.props.timerRun
-            ? <h4>{`${minutes}:${seconds}`}</h4>
-            : <input type='number'
-                value={this.state.interval}
-                autoFocus={true}
-                min={1}
-                max={60}
-                onWheel={this.scroll}
-                onChange={(e) => this.setState({ interval: e.target.value })}></input>;
+        const button = this.props.timerRun ?
+            <Button color='secondary' onClick={this.stop}>Cancel Timer</Button> :
+            <Button color='primary' onClick={this.start}>Start</Button>;
         return (
             <Modal isOpen={this.props.isOpen} toggle={this.props.toggle} size='sm' autoFocus={false} centered={true} >
-                <ModalHeader toggle={this.props.toggle}>Set pause interval</ModalHeader>
-                <ModalBody className='d-flex justify-content-center'>
-                    {comp}
+                <ModalHeader toggle={this.props.toggle}></ModalHeader>
+                <ModalBody>
+                    <div>
+                        <h4 className='text-center'>{`Pause in ${minutes}:${seconds} min`}</h4>
+                        {!this.props.timerRun && <input type='range'
+                            className='custom-range'
+                            value={this.state.interval}
+                            autoFocus={true}
+                            min={30}
+                            max={3600}
+                            step={30}
+                            onWheel={this.scroll}
+                            onChange={(e) => this.setState({ interval: e.target.value })}></input>}
+
+                    </div>
                 </ModalBody>
-                <ModalFooter className='d-flex justify-content-between'>
-                    <Button color='secondary' onClick={this.props.toggle}>Cancel</Button>
-                    <Button color='primary' onClick={this.props.timerRun ? this.stop : this.start}>{this.props.timerRun ? 'Stop' : 'Start'}</Button>
+                <ModalFooter className='d-flex justify-content-center'>
+                    {button}
                 </ModalFooter>
             </Modal>
         );
