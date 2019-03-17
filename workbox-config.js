299e3ea94d0e25f692902c333cf26d1b3a30eeef
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-03-17 10:36:49 +0200

    workbox configuration

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  "globDirectory": "build/",
-  "globPatterns": [
-    "**/*.{png,ico,html,js,css,mp3,gif,svg}"
+  globDirectory: "build/",
+  globPatterns: [
+    "**/*.{html,js,css,mp3,gif,svg}"
   ],
-  "swDest": "build/service-worker-custom.js",
-  "maximumFileSizeToCacheInBytes":"5242880"
+  swDest: "build/service-worker-custom.js",
+  maximumFileSizeToCacheInBytes: "5242880",
+  clientsClaim: true,
+  navigateFallback: '/index.html',
+  navigateFallbackBlacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  globIgnores: ["**/service-worker.js",
+    "**/precache-manifest*"]
 };
