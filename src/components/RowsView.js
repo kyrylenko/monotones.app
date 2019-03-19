41c51f1085b05bc1756a3e23d9d2dadbcde163d3
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-03-19 13:43:19 +0200

    RowsView => functional component

--- src/components/RowsView.js
+++ src/components/RowsView.js
@@ -1,60 +1,57 @@
-import React, { Component } from 'react';
-import { Col, Container, Row } from 'reactstrap'; //https://github.com/react-bootstrap/react-bootstrap
+import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
 import PropTypes from 'prop-types'
 import { SoundSlider } from '../components/SoundSlider';
 
-export class RowsView extends Component {
+const RowsView = (props) => {
 
-    //isOdd(num) { return (num % 2) === 1; }
+    const elements = [];
 
-    render() {
-        //console.log("RowsView props ", this.props);
+    for (let i = 0; i < props.sounds.length - 1; i += 2) {
 
-        let elements = [];
+        const j = i + 1;
+        const sLeft = props.sounds[i];
+        const sRight = props.sounds[j];
 
-        for (let i = 0; i < this.props.sounds.length - 1; i += 2) {
+        const row = <Row key={i} className='sounds-row'>
+            <Col lg={2} md={2} sm={2} className='d-none d-sm-block'></Col>
+            <Col lg={3} md={3} sm={4} xs={6} className='text-center'>
+                <SoundSlider
+                    id={sLeft.id}
+                    isGlobalPlay={props.isGlobalPlay}
+                    isPlay={sLeft.isPlay}
+                    volume={sLeft.volume}
+                    title={sLeft.id}
+                    playPauseVolume={props.playPauseVolume} />
+            </Col>
+            <Col lg={2} md={2} sm={2} className='d-none d-sm-none d-md-block'></Col>
+            <Col lg={3} md={3} sm={4} xs={6} className='text-center'>
+                <SoundSlider
+                    id={sRight.id}
+                    isGlobalPlay={props.isGlobalPlay}
+                    isPlay={sRight.isPlay}
+                    volume={sRight.volume}
+                    title={sRight.id}
+                    playPauseVolume={props.playPauseVolume} />
+            </Col>
+            <Col lg={2} md={2} sm={2} className='d-none d-sm-block'></Col>
+        </Row>;
 
-            let j = i + 1;
-            let sLeft = this.props.sounds[i];
-            let sRight = this.props.sounds[j];
-
-            let row = <Row key={i} className="sounds-row">
-                <Col lg={2} md={2} sm={2} className="d-none d-sm-block"></Col>
-                <Col lg={3} md={3} sm={4} xs={6} className="text-center">
-                    <SoundSlider
-                        id={sLeft.id}
-                        isGlobalPlay={this.props.isGlobalPlay}
-                        isPlay={sLeft.isPlay}
-                        volume={sLeft.volume}
-                        title={sLeft.id}
-                        playPauseVolume={this.props.playPauseVolume} />
-                </Col>
-                <Col lg={2} md={2} sm={2} className="d-none d-sm-none d-md-block"></Col>
-                <Col lg={3} md={3} sm={4} xs={6} className="text-center">
-                    <SoundSlider
-                        id={sRight.id}
-                        isGlobalPlay={this.props.isGlobalPlay}
-                        isPlay={sRight.isPlay}
-                        volume={sRight.volume}
-                        title={sRight.id}
-                        playPauseVolume={this.props.playPauseVolume} />
-                </Col>
-                <Col lg={2} md={2} sm={2} className="d-none d-sm-block"></Col>
-            </Row>;
-
-            elements.push(row);
-        }
-
-        return (
-            <Container>                            
-                {elements}
-            </Container>
-        );
+        elements.push(row);
     }
+
+    return (
+        <Container>
+            {elements}
+        </Container>
+    );
+
 }
 
 RowsView.propTypes = {
     playPauseVolume: PropTypes.func.isRequired,
     isGlobalPlay: PropTypes.bool.isRequired,
     sounds: PropTypes.array.isRequired
-  }
+}
+
+export default RowsView;
