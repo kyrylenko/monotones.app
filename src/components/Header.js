20c27234bcddd53ff8fc679c160a0d10d062d918
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-03-27 21:01:57 +0200

    clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,16 +12,14 @@ const Header = (props) => {
         <header className='container-fluid'>
             <div className='row justify-content-end'>
                 <div className='col-4'>
-                    <div className='app-header'>
-                        <Link to='/' title={constants.appName}>
-                            <img src={logo} className='app-logo' alt={constants.appName} />
-                        </Link>
-                        <h1 className='app-title'>{constants.appName}</h1>
-                    </div>
+                    <Link to='/' title={constants.appName}>
+                        <img src={logo} className='app-logo' alt={constants.appName} />
+                    </Link>
+                    <h3>{constants.appName}</h3>
                 </div>
-                <div className='col-4 d-flex justify-content-around app-title align-items-center'>
-                    <Share reduxSounds={props.reduxSounds} className='top-bar-item' />
-                    <Link to='/donate' className='top-bar-item' title='Buy us a cup of coffee :)'>Donate</Link>
+                <div className='col-4 d-flex justify-content-end align-items-center app-title'>
+                    <Share reduxSounds={props.reduxSounds} className='top-bar-item px-lg-5 px-4' />
+                    <Link to='/donate' className='top-bar-item px-lg-5 px-4' title='Buy us a cup of coffee :)'>Donate</Link>
                 </div>
             </div>
         </header>
