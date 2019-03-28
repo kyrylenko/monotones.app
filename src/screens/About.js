ce772e715ab682474234cd90a915df81c509d752
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-03-28 14:55:47 +0200

    corrected text and html of about and terms

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,11 +3,33 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-            <main className='container about-text'>
-                <h2>Your subtle companion.</h2>
-                <p>
-                    Feeling that the noise of the big city is too much for you to bear? Just turn on the sound and allow yourself to become engulfed in the tranquil sounds of nature. Whether you wish to feel as if you’re sitting near a fireplace under a cozy blanket, or that you’re meditating on a desolate sea shore as gusts of wind ruffle your hair, Monotones will be there to set the mood. With this ambient sound equalizer, not only will you be able to relax, but you will also increase your productivity, as you’re trying to concentrate on your work!
-                </p>
+            <main style={{ textAlign: 'left' }} className='container about-text'>
+                <section>
+                    <h3>Your subtle companion.</h3>
+                    <p>Feeling that the noise of the big city is too much for you to bear? Just turn on the sound and allow yourself to become engulfed in the tranquil sounds of nature. Whether you wish to feel as if you’re sitting near a fireplace under a cozy blanket, or that you’re meditating on a desolate sea shore as gusts of wind ruffle your hair, Monotones will be there to set the mood. With this ambient sound equalizer, not only will you be able to relax, but you will also increase your productivity, as you’re trying to concentrate on your work!</p>
+                </section>
+                <section>
+                    <h4>We Are The Creators</h4>
+                    <p>Pavlo - happy father of beautiful baby girl and caring husband in his amazing family (cute painted image) and Anastasiya - UX designer, ambitious about time-management and anxious to find harmony between work and rest.</p>
+                    <p>We are two people, coming from different branches and with different issues, but who found a mutual solution not only for us but for millions of other people - the impact of music on the ability to correct brain performance. </p>
+                    <p>Either to calm a baby in the middle of the night or to boost a sleepy and non-productive conscious at 2 p.m., we realized that the best option is kept in non-figurative sounds, which do not take over brain activity but subtly help to correct it in the preferable direction.</p>
+                    <p>Every sound in our collection was tested and we proved an impact with our own experience. As well, we believe in other creators, who can create perfect sound combination either to find a harmony between inner and outer, have a conversation with oneself or to motivate one's brain cells to focus on the important task.</p>
+                    <p>We are the creators of our perfect world and we would like to share it with you.</p>
+                </section>
+                <section>
+                    <h4>Vision</h4>
+                    <p>We believe, that there is always a way to elevate the quality of one's life and we know, how to do it. With Monotones we collected and continue to collect the most relevant and useful sounds together with developing new functionality for you to combine our tools with your creativity and not only help you to find inner peace, but share your experience with others, forming a community of beautiful and mindful people.</p>
+                </section>
+                <section>
+                    <h4>Mission</h4>
+                    <p>Among crazy amount of anger, frustration, and anxiousness we built an island of tones. We can see, how those tones help us in everyday life and want to share that experience of better living. We analyzed every other solution, tested it and built our own insight, which helped to increase usability and usefulness of Monotones. Now we want to spread it among different communities to simply help hundreds of other people to live a better life. Guess, this is how we see our mission of making this world a better place to live.</p>
+                </section>
+                <section>
+                    <h4>Values</h4>
+                    <p>We value innovativeness, as we always crave to find the best cognitive solution to achieve needed results and turn into instrument, which would be easy to use.</p>
+                    <p>We appreciate respect, as this is what we feel towards our users. That is why we decided not to ever distract aesthetics of sound and image with any kind of advertisement or anything else, what may cause anxiousness and frustration.</p>
+                    <p>We believe in creativity as we are visionaries. This is why we not only collected the best sounds and tested them, but created a beautiful visual world, which brings a feeling of calmness and gives a perception of harmony.</p>
+                </section>
             </main>
         );
     }
