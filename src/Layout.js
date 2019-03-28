f6be46f523fe5b26b7694f8ec3ccbd6981e2a84e
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-03-28 15:02:33 +0200

    refactor

--- src/Layout.js
+++ src/Layout.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
 import './App.css';
 
-class Layout extends Component {
+const Layout = (props) => {
 
-  render() {
-    return (
-      <div className='app'>
-        <Header reduxSounds={this.props.reduxSounds} isMobile={this.props.isMobile} />
-        {this.props.children}
-        <footer className='container footer-logo-welcome'>
-          <div className='row'>
-            <nav className='col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-logo-menu'>
-              <ul style={{ marginBottom: 0 }}>
-                {/* <li><a target='_blank' rel='noopener noreferrer' href='/about'>About/Features</a></li> */}
-                <li><Link to='/about' >About</Link> </li>
-                <li><Link to='/terms' >Terms &amp; Privacy</Link> </li>
-                <li><a target='_blank' rel='noopener noreferrer' href='mailto:contact@monotones.app'>Contact</a></li>
-              </ul>
-            </nav>
-          </div>
-        </footer>
-      </div>
-    );
-  }
+  return (
+    <div className='app'>
+      <Header reduxSounds={props.reduxSounds} isMobile={props.isMobile} />
+      {props.children}
+      <footer className='container footer-logo-welcome'>
+        <div className='row'>
+          <nav className='col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-logo-menu'>
+            <ul style={{ marginBottom: 0 }}>
+              <li><Link to='/about' >About</Link> </li>
+              <li><Link to='/terms' >Terms &amp; Privacy</Link> </li>
+              <li><a target='_blank' rel='noopener noreferrer' href='mailto:contact@monotones.app'>Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 
