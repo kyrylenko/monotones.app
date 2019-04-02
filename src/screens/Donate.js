3a1a5045208122f85ccdaf5361f92a8b65e122b6
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-04-02 17:19:55 +0300

    tiny

--- src/screens/Donate.js
+++ src/screens/Donate.js
@@ -14,7 +14,7 @@ const Donate = () => {
                     <input type="hidden" name="cmd" value="_s-xclick" />
                     <input type="hidden" name="hosted_button_id" value="5BERTKKK3B78J" />
                     <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                    <img alt="" border="0" src="https://www.paypal.com/en_UA/i/scr/pixel.gif" width="1" height="1" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
                 </form>
             </section>
             <section>
