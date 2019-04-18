16a0d111fb03954155385641cae52a43bc1c97e5
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-04-18 21:56:02 +0300

    correction

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import About from './screens/About';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<About />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
