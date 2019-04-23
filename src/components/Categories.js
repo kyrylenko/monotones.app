2ce005ffc3fe882a1e1913df4c66f564b07a906f
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-04-23 21:14:45 +0300

    styles, clean up

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 const CategoriesWeb = (props) => {
     return (
-        <div className='list-group' style={{ width: '80px', position: 'fixed', right: 0 }}>
+        <div className='list-group' style={{ width: '80px', height: '47px', position: 'fixed', right: 0 }}>
             <NavLink exact to='/' title='All' className='list-group-item list-group-item-action' activeClassName='active'>All</NavLink>
             <NavLink exact to='/relax' title='Relax' className='list-group-item list-group-item-action' activeClassName='active'>Relax</NavLink>
             <NavLink exact to='/sleep' title='Sleep' className='list-group-item list-group-item-action' activeClassName='active'>Sleep</NavLink>
@@ -14,10 +14,10 @@ const CategoriesWeb = (props) => {
 
 const CategoriesMobile = (props) => {
     return (
-        <div className='list-group list-group-horizontal' style={{position: 'sticky'}}>
-            <NavLink exact to='/' title='All' className='list-group-item flex-fill' activeClassName='active'>All</NavLink>
-            <NavLink exact to='/relax' title='Relax' className='list-group-item flex-fill' activeClassName='active'>Relax</NavLink>
-            <NavLink exact to='/sleep' title='Sleep' className='list-group-item flex-fill' activeClassName='active'>Sleep</NavLink>
+        <div className='list-group list-group-horizontal' style={{position: 'sticky', height: '44px'}}>
+            <NavLink exact to='/' title='All' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>All</NavLink>
+            <NavLink exact to='/relax' title='Relax' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>Relax</NavLink>
+            <NavLink exact to='/sleep' title='Sleep' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>Sleep</NavLink>
         </div>
     );
 }
