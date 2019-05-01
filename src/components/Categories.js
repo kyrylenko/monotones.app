6288c847f02fe38a894f6f653f585c082b8c8aef
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-05-01 18:52:19 +0300

    reorder categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,8 +6,8 @@ const CategoriesWeb = (props) => {
     return (
         <div className='list-group' style={{ width: '80px', height: '47px', position: 'fixed', right: 0 }}>
             <NavLink exact to='/' title='All' className='list-group-item list-group-item-action' activeClassName='active'>All</NavLink>
-            <NavLink exact to='/relax' title='Relax' className='list-group-item list-group-item-action' activeClassName='active'>Relax</NavLink>
             <NavLink exact to='/sleep' title='Sleep' className='list-group-item list-group-item-action' activeClassName='active'>Sleep</NavLink>
+            <NavLink exact to='/relax' title='Relax' className='list-group-item list-group-item-action' activeClassName='active'>Relax</NavLink>            
             <NavLink exact to='/focus' title='Focus' className='list-group-item list-group-item-action' activeClassName='active'>Focus</NavLink>
         </div>
     );
@@ -17,8 +17,8 @@ const CategoriesMobile = (props) => {
     return (
         <div className='list-group list-group-horizontal' style={{position: 'sticky', height: '44px'}}>
             <NavLink exact to='/' title='All' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>All</NavLink>
-            <NavLink exact to='/relax' title='Relax' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>Relax</NavLink>
             <NavLink exact to='/sleep' title='Sleep' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>Sleep</NavLink>
+            <NavLink exact to='/relax' title='Relax' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>Relax</NavLink>            
             <NavLink exact to='/focus' title='Focus' className='list-group-item list-group-item-action flex-fill' activeClassName='active'>Focus</NavLink>
         </div>
     );
