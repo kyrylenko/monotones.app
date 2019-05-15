7b33ee58b30f3e79c52980f3327890f82e3fc23b
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-05-15 11:40:39 +0300

    mobile categories pills

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -29,10 +29,16 @@ export const CategoriesWeb = ({ pathname }) => {
 
 export const CategoriesMobile = ({ pathname }) => {
     return (
-        <div className='list-group list-group-horizontal' style={{ position: 'sticky', height: '44px' }}>
-            <Link to={getPath(pathname, '/sleep')} title='Sleep' className={`list-group-item list-group-item-action flex-fill ${getClass(pathname, '/sleep')}`}>Sleep</Link>
-            <Link to={getPath(pathname, '/relax')} title='Relax' className={`list-group-item list-group-item-action flex-fill ${getClass(pathname, '/relax')}`}>Relax</Link>
-            <Link to={getPath(pathname, '/focus')} title='Focus' className={`list-group-item list-group-item-action flex-fill ${getClass(pathname, '/focus')}`}>Focus</Link>
+        <div className='nav nav-pills' style={{ position: 'sticky', height: '44px' }}>
+            <li className='nav-item flex-fill'>
+                <Link to={getPath(pathname, '/sleep')} title='Sleep' className={`nav-link ${getClass(pathname, '/sleep')}`}>Sleep</Link>
+            </li>
+            <li className='nav-item flex-fill'>
+                <Link to={getPath(pathname, '/relax')} title='Relax' className={`nav-link ${getClass(pathname, '/relax')}`}>Relax</Link>
+            </li>
+            <li className='nav-item flex-fill'>
+                <Link to={getPath(pathname, '/focus')} title='Focus' className={`nav-link ${getClass(pathname, '/focus')}`}>Focus</Link>
+            </li>
         </div>
     );
 }
