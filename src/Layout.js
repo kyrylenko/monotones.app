7b7fabea375f7a5cd366659f34d6b19f457411fb
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-05-15 12:52:12 +0300

    nav hover stypes

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Header from './components/Header';
 import background from './assets/icons/background.png';
 import './App.css';
@@ -18,8 +18,8 @@ const Layout = (props) => {
           <div className='row py-3 py-md-5'>
             <nav className='col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-menu'>
               <ul style={{ marginBottom: 0 }} className='px-0'>
-                <li><Link to='/about' >About</Link> </li>
-                <li><Link to='/terms' >Terms</Link> </li>
+                <li><NavLink to='/about' >About</NavLink> </li>
+                <li><NavLink to='/terms' >Terms</NavLink> </li>
                 <li><a target='_blank' rel='noopener noreferrer' href='mailto:contact@monotones.app'>Contact</a></li>
               </ul>
             </nav>
