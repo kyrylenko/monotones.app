54924efa278bfe76edc7dfe15510090f8a22c8fc
Kyrylenko <kirilenko.pavlo@gmail.com>
2019-05-15 13:46:00 +0300

    slider styles

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => (
-    <div style={{ textAlign: 'left' }} className='container about-text'>
+    <div style={{ textAlign: 'left' }} className='container about-text violet-headers'>
         <section>
             <h3>Your subtle companion.</h3>
             <p>Feeling that the noise of the big city is too much for you to bear? Just turn on the sound and allow yourself to become engulfed in the tranquil sounds of nature. Whether you wish to feel as if you’re sitting near a fireplace under a cozy blanket, or that you’re meditating on a desolate sea shore as gusts of wind ruffle your hair, Monotones will be there to set the mood. With this ambient sound equalizer, not only will you be able to relax, but you will also increase your productivity, as you’re trying to concentrate on your work!</p>
