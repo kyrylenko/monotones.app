3632163291b26f87b0d161660196873b02f29bc9
Pavlo Kyrylenko <pkyrylenko@vector-software.com>
2020-08-10 19:14:21 +0300

    liquipay widget in donate

--- src/screens/Donate.js
+++ src/screens/Donate.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //TODO: donations using PayPal https://github.com/paypal/paypal-checkout-components
 //https://github.com/thinhvo0108/react-paypal-express-checkout
 // & BTC http://www.reactjstutorial.net/donate.html
 
-export default () => (
-    <section className='mt-5'>
-        <p>If you're willing to donate, please
-            <a className="bmc-button" target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/kyrylenko">
-                <img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy us a coffee"></img>
-                <span style={{ marginLeft: '5px' }}>Buy us a coffee</span>
-            </a>!
-        </p>
-        <p>Or</p>
-        <p>just contact us! <a target='_blank' rel='noopener noreferrer' href='mailto:contact@monotones.app'>contact@monotones.app</a></p>
-        {/* <h5>PayPal</h5>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                <input type="hidden" name="cmd" value="_s-xclick" />
-                <input type="hidden" name="hosted_button_id" value="5BERTKKK3B78J" />
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-            </form> */}
-    </section>
-);
+class Donate extends Component {
+
+    componentDidMount() {
+        window.LiqPayCheckoutCallback = function() {
+            window.LiqPayCheckout.init({
+              data: "eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXlkb25hdGUiLCJhbW91bnQiOiI1MCIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiJTdXBwb3J0IE1vbm90b25lcyEiLCJwdWJsaWNfa2V5IjoiaTc2NzM0NTIxNjk0IiwibGFuZ3VhZ2UiOiJlbiJ9",
+              signature: "gWGx3NfrzfuuW6pedgXDjfY0HXw=",
+              embedTo: "#liqpay_checkout",
+              mode: "embed" // embed || popup,
+                }).on("liqpay.callback", function(data){
+              console.log(data.status);
+              console.log(data);
+              }).on("liqpay.ready", function(data){
+                // ready
+              }).on("liqpay.close", function(data){
+                // close
+            });
+          };
+
+        window.LiqPayCheckoutCallback();
+    }
+
+    render() {
+        return (<section className='mt-5'>            
+            <p>If you like Monotones, buy us a coffee ;)</p>
+            <div id="liqpay_checkout"></div>
+            {/* <a href='https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXlkb25hdGUiLCJhbW91bnQiOiIxMCIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiJEb25hdGUiLCJwdWJsaWNfa2V5IjoiaTc2NzM0NTIxNjk0IiwibGFuZ3VhZ2UiOiJydSJ9&signature=pmyA21MndqhIHkacm+48A6LfOAI='>Liq</a> */}
+        </section>);
+    }
+}
+
+export default Donate;
