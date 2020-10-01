ff76ed8cf5f199def92d6613f202ecc970b88ac8
Kyrylenko <kirilenko.pavlo@gmail.com>
2020-10-01 22:18:59 +0300

    translate about

--- src/screens/About.js
+++ src/screens/About.js
@@ -14,18 +14,16 @@ const About = () => {
             <p className='line-breaks'>{t('we_are_creators_content')}</p>
         </section>
         <section>
-            <h4>Vision</h4>
-            <p>We believe, that there is always a way to elevate the quality of one's life and we know, how to do it. With Monotones we collected and continue to collect the most relevant and useful sounds together with developing new functionality for you to combine our tools with your creativity and not only help you to find inner peace, but share your experience with others, forming a community of beautiful and mindful people.</p>
+            <h4>{t('vision')}</h4>
+            <p>{t('vision_content')}</p>
         </section>
         <section>
-            <h4>Mission</h4>
-            <p>Among crazy amount of anger, frustration, and anxiousness we built an island of tones. We can see, how those tones help us in everyday life and want to share that experience of better living. We analyzed every other solution, tested it and built our own insight, which helped to increase usability and usefulness of Monotones. Now we want to spread it among different communities to simply help hundreds of other people to live a better life. Guess, this is how we see our mission of making this world a better place to live.</p>
+            <h4>{t('mission')}</h4>
+            <p>{t('mission_content')}</p>
         </section>
         <section>
-            <h4>Values</h4>
-            <p>We value innovativeness, as we always crave to find the best cognitive solution to achieve needed results and turn into instrument, which would be easy to use.</p>
-            <p>We appreciate respect, as this is what we feel towards our users. That is why we decided not to ever distract aesthetics of sound and image with any kind of advertisement or anything else, what may cause anxiousness and frustration.</p>
-            <p>We believe in creativity as we are visionaries. This is why we not only collected the best sounds and tested them, but created a beautiful visual world, which brings a feeling of calmness and gives a perception of harmony.</p>
+            <h4>{t('values')}</h4>
+            <p>{t('values_content')}</p>
         </section>
     </div>
 };
