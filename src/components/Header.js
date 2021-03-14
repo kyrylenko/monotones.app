b0283910e39123ee12843ac9660e8b3baab70deb
Pavlo Kyrylenko <kirilenko.pavlo@gmail.com>
2021-03-14 18:25:25 +0200

    tiny correction

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Share from '../components/Share';
 import constants from '../constants/defaultValues';
 import logo from '../logo.png';
@@ -18,7 +18,7 @@ const HeaderWeb = (props) => {
                 </div>
                 <div className='col-4 d-flex justify-content-end align-items-center app-title'>
                     <Share reduxSounds={props.reduxSounds} className='top-bar-item px-lg-5 px-4' />                    
-                    <a href='https://www.patreon.com/monotones' target='_blank' rel='noreferrer' rel='noopener' className='top-bar-item px-lg-5 px-4' title={t('buy_us_coffee')}>{t('donate')}</a>
+                    <a href='https://www.patreon.com/monotones' target='_blank' rel='noopener noreferrer' className='top-bar-item px-lg-5 px-4' title={t('buy_us_coffee')}>{t('donate')}</a>
                 </div>
             </div>
         </header>
@@ -33,7 +33,7 @@ const HeaderMobile = (props) => {
             <Link to='/' title={constants.appName}>
                 <img src={logo} alt={constants.appName} style={{ height: '45px' }} />
             </Link>            
-            <a href='https://www.patreon.com/monotones' target='_blank' rel='noreferrer' rel='noopener'className='top-bar-item' title={t('buy_us_coffee')}>{t('donate')}</a>
+            <a href='https://www.patreon.com/monotones' target='_blank' rel='noopener noreferrer' className='top-bar-item' title={t('buy_us_coffee')}>{t('donate')}</a>
         </header>
     );
 }
